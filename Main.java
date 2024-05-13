70cef45d0be1187af096b3e34fa2e0d2cba16782
Gustavo Esteban Falcon <gustavoestebanfalcon@gmail.com>
2024-05-13 14:07:47 -0400

    Basic understanding of packages, constructors, and encapsulation

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+import toilet.Piss;
+public class Main {
+    public static void main(String[] args) {
+        Piss joe = new Piss(31, "joebert");
+        joe.urinate();
+    }
+}
---
+++ toilet/Piss.java
@@ -0,0 +1,22 @@
+package toilet;
+public class Piss {
+    private int pressure;
+    private String ownerName;
+
+    public Piss(int pressure, String ownerName) {
+        this.pressure = pressure;
+        this.ownerName = ownerName;
+    }
+
+    private void squirt() {
+        System.out.println(ownerName + " is squirting");
+    }
+    private void flush() {
+        System.out.println("wshwssshwshwsh");
+    }
+
+    public void urinate() {
+        squirt();
+        flush();
+    }
+}
