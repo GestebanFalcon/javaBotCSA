7e51cad26e096efec8f55b001f3f070d2b0ef652
Gustavo Esteban Falcon <gustavoestebanfalcon@gmail.com>
2024-05-16 18:00:51 -0400

    tried to drop commit with id

--- src/main/java/localhost/Bot.java
+++ src/main/java/localhost/Bot.java
@@ -1,26 +1,53 @@
 package localhost;
+
+import localhost.listeners.MessageReceivedListener;
+import localhost.listeners.ReadyListener;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
-import src.main.java.localhost.toilet.Piss;
+
+import javax.security.auth.login.LoginException;
 
 /**
- * Hello world!
+ * JDA-5.0.0 Discord Bot Tutorial Series.
+ * This is the main class where we initialize our bot.
  *
+ * @author TechnoVision <a href="https://www.youtube.com/TechnoVisionTV">...</a>
  */
-public class Bot 
-{
-    private final ShardManager shardManager
-    public Bot() {
-        DefaultShardManagerBuilder builder = new DefaultShardManagerBuilder.createDefault("");
+public class Bot {
+
+    private final ShardManager shardManager;
+
+    /**
+     * Loads environment variables and builds the bot shard manager.
+     * @throws LoginException occurs when bot token is invalid.
+     */
+    public Bot() throws LoginException {
+        String token = "";
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("GoatVision +"));
+        builder.addEventListeners(new ReadyListener(), new MessageReceivedListener());
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGES);
         shardManager = builder.build();
     }
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        Piss joe = new Piss(31, "joebert");
-        joe.urinate();
-        
+
+    /**
+     * Retrieves the bot shard manager.
+     * @return the ShardManager instance for the bot.
+     */
+    public ShardManager getShardManager() { return shardManager; }
+
+    /**
+     * Main method where we start our bot.
+     */
+    public static void main(String[] args) {
+        try {
+            Bot bot = new Bot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid!");
+        }
     }
 }
